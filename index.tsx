5494a71ccaa5907cde5b8b26180bd4654ff19a21
Mateus Rafael Suss <167236538+MateusRafaelSuss@users.noreply.github.com>
2026-03-25 11:39:08 -0300

    Add Index component with Hello World and Cesta

---
+++ index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text, View } from "react-native";
+import Cesta from "./src/telas/Cesta.js";
+//import Teste from "./src/telas/Teste.js";
+
+
+export default function Index() {
+  return (
+    <View>
+      <Text>Hello, World</Text>
+       <Cesta />
+      
+    </View>
+  );
+}
