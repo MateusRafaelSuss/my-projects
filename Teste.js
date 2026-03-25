97d9221e1100b31d695e7f8b3dfb42a94e0915c2
Mateus Rafael Suss <167236538+MateusRafaelSuss@users.noreply.github.com>
2026-03-25 11:39:36 -0300

    Create Teste.js component with basic structure
    Initial creation of Teste.js component with imports and layout.

---
+++ Teste.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Dimensions, Image, StyleSheet, Text, View } from 'react-native';
+
+import rdrlogo from '../../../assets/reddead/rdrlogo';
+import johnmarston from '../../../assets/reddead/johnmarston.png'
+
+const width = Dimensions.get('screen').width;
+
+export default function Teste(){
+    return (
+          <>
+            Image
+    
+    
+    
+    
+         </>)
+}
+
