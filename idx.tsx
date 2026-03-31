f9df19766e0d028f347f7b53db5079d613ec3312
Mateus Rafael Suss <167236538+MateusRafaelSuss@users.noreply.github.com>
2026-03-31 18:24:43 -0300

    Create Index component to render Curriculo

---
+++ idx.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+import Curriculo from "../../assets/telas/Curriculo";
+
+export default function Index() {
+  return (
+    <View>
+      <Curriculo />
+    </View>
+  );
+}
